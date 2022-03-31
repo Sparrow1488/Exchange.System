5ee5724c732f372d25ccfe71d278be58917a2ef6
Валентин <ilyaokubev@gmail.com>
2022-03-31 22:42:34 +0300

    Review: добавил новый AuthService на клиенте, чтобы почистить код

--- src/Exchange.Sample.Client/Startup.cs
+++ src/Exchange.Sample.Client/Startup.cs
@@ -1,8 +1,4 @@
-﻿using Exchange.System.Entities;
-using Exchange.System.Enums;
-using Exchange.System.Packages;
-using Exchange.System.Sendlers;
-using ExchangeSystem.Helpers;
+﻿using Exchange.Sample.Client.Abstractions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
@@ -12,61 +8,33 @@ namespace Exchange.Sample.Client
 {
     internal class Startup
     {
-        public Startup(ILogger<Startup> logger, IConfiguration config)
+        public Startup(
+            ILogger<Startup> logger, 
+            IConfiguration config, 
+            IAuthorizationService authorization)
         {
             _logger = logger;
             _config = config;
+            _authorization = authorization;
         }
 
         private readonly ILogger<Startup> _logger;
         private readonly IConfiguration _config;
+        private readonly IAuthorizationService _authorization;
 
         public async Task RunAsync()
         {
             _logger.LogInformation($"{nameof(Startup)} running");
-            for (int i = 0; i < 20; i++)
+            for (int i = 0; i < 3; i++)
             {
-                var connectionSettings = CreateConnectionSettings();
-                var sendler = new NewRequestSendler(connectionSettings);
-                _logger.LogInformation("GET => " + "Authorization");
-                var response = await sendler.SendRequestAsync(CreateEmptyRequest());
-                _logger.LogInformation("STATUS => " + response.Report.Status.ToString());
-                _logger.LogInformation($"MESSAGE => {response.Report.Message}");
-
-                if (response.Report.Status.Equals(AuthorizationStatus.Success))
+                await _authorization.AuthorizeAsync();
+                if (_authorization.IsSuccess())
                 {
-                    var correctResponse = response as Response<Guid>;
-                    Ex.ThrowIfNull(correctResponse);
-                    _logger.LogInformation("GUID => " + correctResponse.Content.ToString());
-                }
-                else if (response.Report.Status.Equals(AuthorizationStatus.Failed))
-                {
-                    _logger.LogWarning("Authorization Failed");
-                }
-                else
-                {
-                    _logger.LogError("Error");
+                    var authToken = _authorization.GetToken();
+                    _logger.LogInformation("Auth token => " + authToken.ToString());
                 }
                 await DelayAsync();
             }
-        }
-
-        private ConnectionSettings CreateConnectionSettings() =>
-            new ConnectionSettings(_config.GetValue<string>("EndpointHost"), 
-                                    _config.GetValue<int>("EndpointPort"));
-
-        private Request CreateAuthorizationRequest()
-        {
-            var request = new Request<UserPassport>("Authorization", ProtectionType.Default);
-            request.Body = new Body<UserPassport>(new UserPassport("asfd", "1234"));
-            return request;
-        }
-
-        private Request CreateEmptyRequest()
-        {
-            var request = new Request<EmptyEntity>("Authorization", ProtectionType.Default);
-            request.Body = new Body<EmptyEntity>(new EmptyEntity());
-            return request;
         }
 
         private async Task DelayAsync() => await Task.Delay(TimeSpan.FromMilliseconds(50));
