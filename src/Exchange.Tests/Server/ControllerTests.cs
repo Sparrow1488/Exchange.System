458e29565487f73622bf8f4c96fe4de02209e146
Валентин <ilyaokubev@gmail.com>
2022-03-31 22:46:02 +0300

    Review: перенес два проекта с тестами в один и разделил не дериктории

---
+++ src/Exchange.Tests/Server/ControllerTests.cs
@@ -0,0 +1,40 @@
+using Exchange.Server.Controllers;
+using Exchange.System.Entities;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace Exchange.Tests.Server
+{
+    public class ControllerTests
+    {
+        public UserPassport ValidPassport { get; private set; }
+        public UserPassport InvalidPassport { get; private set; }
+        public ControllerSelector ControllerSelector { get; private set; }
+
+        [SetUp]
+        public void Setup() 
+        {
+            ValidPassport = new UserPassport("asd", "1234");
+            InvalidPassport = new UserPassport("", null);
+            ControllerSelector = new ControllerSelector();
+        }
+
+        [Ignore("Изменилась логика, в данный момент тест не может быть пройдет, нужно переписать")]
+        [Test]
+        public void ProcessRequest_RequestContextWithDisconnectedClient_ResponsePackage()
+        {
+            //Assert.NotNull(response);
+            //Assert.NotNull(responseReport);
+            //Assert.AreSame(System.Enums.AuthorizationStatus.Success, responseReport.Status);
+        }
+
+        [Ignore("Изменилась логика, в данный момент тест не может быть пройдет, нужно переписать")]
+        [Test]
+        public async Task ProcessRequest_RequestContextWithDisconnectedClientAndInvalidPassport_ResponsePackage()
+        {
+            //Assert.NotNull(response);
+            //Assert.NotNull(responseReport);
+            //Assert.AreSame(System.Enums.ResponseStatus.Bad, responseReport.Status);
+        }
+    }
+}
