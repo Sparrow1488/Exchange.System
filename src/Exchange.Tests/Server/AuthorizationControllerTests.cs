09e0791d49d55333910359af79a57bff1b7596d4
Валентин <ilyaokubev@gmail.com>
2022-04-01 20:51:12 +0300

    Feature: запек второй тестик для контроллера

--- src/Exchange.Tests/Server/AuthorizationControllerTests.cs
+++ src/Exchange.Tests/Server/AuthorizationControllerTests.cs
@@ -1,5 +1,7 @@
+using Exchange.Server.Controllers;
 using Exchange.Server.Primitives;
 using Exchange.System.Entities;
+using Exchange.System.Enums;
 using Exchange.System.Packages;
 using Exchange.Tests.Server.Mocks.Controllers;
 using ExchangeSystem.Packages;
@@ -10,26 +12,56 @@ namespace Exchange.Tests.Server
 {
     public class AuthorizationControllerTests
     {
-        private Request<UserPassport> _request;
-        private RequestContext _requestContext;
-        private ResponseReport _responseReport;
+        private AuthorizationController _controllerUnderTests;
 
         [SetUp]
-        public void Setup() 
-        { 
-            _request = new Request<UserPassport>("Authorization");
-            _request.Body = new Body<UserPassport>(new UserPassport("asd", "1234"));
-            _requestContext = RequestContext.ConfigureContext(context => context.SetRequest(_request));
+        public void Setup() { }
+
+        [Test]
+        [TestCase("asd", "1234")]
+        public void Authorize_ValidUserPassport_AuthTokenAndSuccessStatus(
+            string login, string password)
+        {
+            var report = CreateResponseReport(AuthorizationStatus.Success);
+            var expected = new Response<Guid>(report, Guid.NewGuid());
+            var context = CreateRequestContextWithUserPassport(login, password);
+            _controllerUnderTests = AuthorizationControllerMoqFactory.Order(expected, context);
+
+            var authResult = _controllerUnderTests.Authorization();
+            Assert.IsAssignableFrom<Response<Guid>>(authResult);
+            Assert.AreEqual(AuthorizationStatus.Success , authResult.Report.Status);
         }
 
         [Test]
-        public void Authorize_ValidUserPassport_AuthToken()
+        [TestCase(null, "")]
+        public void Authorize_InvalidUserPassport_EmptyEntityAndFailedStatus(
+            string login, string password)
         {
-            var expected = new Response<Guid>(_responseReport, Guid.NewGuid());
-            var controllerUnderTests = AuthorizationControllerMoqFactory.Order(expected, _requestContext);
+            var report = CreateResponseReport(AuthorizationStatus.Failed);
+            var expected = new Response<EmptyEntity>(report, new EmptyEntity());
+            var requestContext = CreateRequestContextWithUserPassport(login, password);
+            _controllerUnderTests = AuthorizationControllerMoqFactory.Order(expected, requestContext);
 
-            var authResponse = controllerUnderTests.Authorization();
-            Assert.IsAssignableFrom<Response<Guid>>(authResponse);
+            var authResult = _controllerUnderTests.Authorization();
+            Assert.IsAssignableFrom<Response<EmptyEntity>>(authResult);
+            Assert.AreEqual(AuthorizationStatus.Failed, authResult.Report.Status);
+        }
+
+        private RequestContext CreateRequestContextWithUserPassport(string login, string passport)
+        {
+            var request = new Request<UserPassport>("Authorization", ProtectionType.Default);
+            request.Body = new Body<UserPassport>(new UserPassport(login, passport));
+            return RequestContext.ConfigureContext(context => context.SetRequest(request));
+        }
+
+        private ResponseReport CreateResponseReport(AuthorizationStatus status, string message = null)
+        {
+            ResponseReport report = default;
+            if (status.Equals(AuthorizationStatus.Failed))
+                report = new ResponseReport(message ?? AuthorizationStatus.Failed.Message, AuthorizationStatus.Failed);
+            else if (status.Equals(AuthorizationStatus.Success))
+                report = new ResponseReport(message ?? AuthorizationStatus.Failed.Message, AuthorizationStatus.Success);
+            return report;
         }
     }
 }
